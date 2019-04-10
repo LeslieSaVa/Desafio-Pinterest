464e26a1b7c625613c20d7d5397a8983047bb387
LeslieSaVa <leslie.sabattin@usach.cl>
2019-04-10 17:33:31 -0300

    Updates

---
+++ precache-manifest.f9c05bb156c58a8709e08f4fcc404af2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9531d4e8d64703c03713",
+    "url": "/Desafio-Pinterest/static/css/main.a677e4c2.chunk.css"
+  },
+  {
+    "revision": "9531d4e8d64703c03713",
+    "url": "/Desafio-Pinterest/static/js/main.aedb8cae.chunk.js"
+  },
+  {
+    "revision": "c5b0f7354d2b9ae88b7a",
+    "url": "/Desafio-Pinterest/static/js/runtime~main.624e1130.js"
+  },
+  {
+    "revision": "2310902dea5dc0625e05",
+    "url": "/Desafio-Pinterest/static/js/2.b9f93adb.chunk.js"
+  },
+  {
+    "revision": "969520bd9d248abbfd40b160e32d61c9",
+    "url": "/Desafio-Pinterest/index.html"
+  }
+];
